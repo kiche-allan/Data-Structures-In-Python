2f76613b87724f0693df71db84962992a4c7c65a
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-16 10:30:28 +0300

    recursion7

--- Recursion/usecase.py
+++ Recursion/usecase.py
@@ -5,3 +5,6 @@ class Node:
         #the above line defines a class 'node' and a method '__init__' that takes three arguments of vaue, left and right. the left and right arguments are optional and default to None. 'value' is the value of the node, 'left' is a reference of the left child node and 'right' is a reference to the right child node. 
         
         #self is not an explicit argument in the above code because it is automatically passed by Python to a method when it is called on an instance of a class. 
+        
+        #in the above code, 'self' is passed to the '__init__' and 'print_tree' method as the first argument when it is called on an instance of the class 'Node'.
+        #'self'is used to refer to the current isntance of the class This allows the method to set the value of the instance variables, 'self.value', 'self.left' and 'self.right' to the values passed to the '__init__' method.
