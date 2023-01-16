d42e5633ff0b357ba2395d7ed664030c4fa2cb9b
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-16 07:31:29 +0300

    recursive 2

--- Recursion/recursion1.py
+++ Recursion/recursion1.py
@@ -1,1 +1,5 @@
-#Recursion is a programming technique where a function calls itself in order to solve a problem.
+#Recursion is a programming technique where a function calls itself in order to solve a problem. 
+#It is often used toperform operations on data structures such as lists or trees where the problem can be broken down into smaller subproblems that are similar in nature to the original problem but smaller in size. A recursive function must have a base case and a recursive case. The base case is the condition that stops the recursion. The recursive case is the condition that calls the function again.
+
+#base case
+#recursive case
