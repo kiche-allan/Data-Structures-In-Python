ca15cbdf75693e89dd65a7bade7668cd39141ff5
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-16 10:31:38 +0300

    recursion7

--- Recursion/usecase.py
+++ Recursion/usecase.py
@@ -8,3 +8,7 @@ class Node:
         
         #in the above code, 'self' is passed to the '__init__' and 'print_tree' method as the first argument when it is called on an instance of the class 'Node'.
         #'self'is used to refer to the current isntance of the class This allows the method to set the value of the instance variables, 'self.value', 'self.left' and 'self.right' to the values passed to the '__init__' method.
+        
+        #  Similarly, in the print_tree method, self is used to refer to the current instance of the class and access its attributes such as value, left, and right.
+        
+        #In python, self is used as a way to refer to the instance of a class, this is why it's not necessary to pass it as an explicit argument. It is automatically passed by Python when a method is called on an instance of a class.
