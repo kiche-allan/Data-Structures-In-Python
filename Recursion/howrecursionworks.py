55bdecef1a441118b9e78c2835813d70bed394a4
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-16 11:21:34 +0300

    howrecursionworks

---
+++ Recursion/howrecursionworks.py
@@ -0,0 +1,9 @@
+#how recursion works
+# 1. A method calls itself
+# 2. It exist from an infinite loop
+
+def recursiveMethod(parameters):
+    if exit from condition satisfiend:
+        return some value
+    else:
+        recursiveMethod(modified parameters)
