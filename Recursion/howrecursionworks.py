6c76c55b876dd3a997e0819883a189fc94749aea
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-16 22:31:13 +0300

    readme

--- Recursion/howrecursionworks.py
+++ Recursion/howrecursionworks.py
@@ -2,8 +2,11 @@
 # 1. A method calls itself
 # 2. It exist from an infinite loop
 
+from runpy import _ModifiedArgv0
+
+
 def recursiveMethod(parameters):
     if exit from condition satisfiend:
         return some value
     else:
-        recursiveMethod(modified parameters)
+        recursiveMethod(_ModifiedArgv0 parameters)
