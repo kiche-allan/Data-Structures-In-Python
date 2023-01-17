66d788a6728c83db1724d8e67a14b26aef20279e
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-17 09:42:19 +0300

    explanation

--- Recursion/fibonacci.py
+++ Recursion/fibonacci.py
@@ -20,3 +20,26 @@ def fibonacci(n):
     
 for i in range(10):
     print(fibonacci(i), end = " ")
+    
+    #explanation of every line of code
+    
+    #the above line defines a function called fibonacci that takes a single argument n and returns the nth fibonacci number. 
+    def fibonacci(n):
+        
+    # The function has two base cases: when n is 0 or 1, it either returns 0 or 1. the recursive case is when n is greater than 1, in which case the function calls itself twice, once with n -1 and once with n-2, and adds the results together. The function keeps calling itself until the base case is reached, and then returns the result of the addition.
+            # Base case: the first two Fibonacci numbers are 0 and 1
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        
+        # These lines serve as the base case for recursion, where the recursion stops. The first base case returns 0 when the input value n is 0, as the first Fibonacci number is 0. The second base case returns 1 when the input value n is 1, as the second Fibonacci number is 1.
+        
+            # Recursive case: the nth Fibonacci number is the sum of the (n-1)th and (n-2)th Fibonacci numbers
+        else:
+            return fibonacci(n-1) + fibonacci(n-2)
+        
+
+        
+
+    
