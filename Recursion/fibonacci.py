df5944dc1bafc34483fccf793c8f5113b0e53246
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-17 09:18:44 +0300

    fibonnacci

---
+++ Recursion/fibonacci.py
@@ -0,0 +1,1 @@
+#the fibonacci numbers are a sequence of numbers in which each number is the sum of the two preceding ones, usually starting with 0 and 1. The sequence goes like: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
