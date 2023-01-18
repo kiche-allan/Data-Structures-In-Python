4ee094cc2a42530b164d93de5d9588ce0f62fe54
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-18 08:12:14 +0300

    fibonacci

--- Recursion/509- Fibonacci number.py
+++ Recursion/509- Fibonacci number.py
@@ -4,14 +4,13 @@
 # F(n) = F(n - 1) + F(n - 2), for n > 1.
 # Given n, calculate F(n)
 
-def fib(n: int) -> int:
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    
-    else:
-        fib_list = [0, 1]
-        for i in range(2, n +1):
-            fib_list.append(fib_list[i-1] + fib_list[i-2])
-        return fib_list(n)
+# //using recursion
+class Solution:
+    def fib(self, n: int) -> int:
+
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+
+        return(self.fib(n-1) + self.fib(n-2)) 
