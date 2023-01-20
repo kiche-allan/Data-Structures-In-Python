3b73074522b3986146a91e76257f08a037a55526
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-20 16:21:41 +0300

    theta

---
+++ Big O Notation/Theta.py
@@ -0,0 +1,5 @@
+# Theta notation (Θ notation) is a way of expressing the upper and lower bounds of an algorithm's running time complexity. It provides a tight bound on the growth of the running time of an algorithm. The running time of an algorithm is often expressed using big O notation, which provides an upper bound on the growth of the running time. However, big O notation can be too loose, and Theta notation provides a tighter bound by expressing both the upper and lower bounds.
+
+# The notation is used to describe the time complexity of an algorithm and is typically represented by the Greek letter Theta (Θ) followed by a mathematical expression. The mathematical expression represents the function that describes the running time of the algorithm.
+
+
