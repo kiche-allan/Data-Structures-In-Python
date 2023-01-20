6f4ff822df82538ef7a626d89beaa71a83d3826c
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-20 16:27:39 +0300

    theta2

--- Big O Notation/Theta.py
+++ Big O Notation/Theta.py
@@ -3,3 +3,9 @@
 # The notation is used to describe the time complexity of an algorithm and is typically represented by the Greek letter Theta (Θ) followed by a mathematical expression. The mathematical expression represents the function that describes the running time of the algorithm.
 
 
+def find_max(arr):
+    max_val = arr[0]
+    for i in range(1 len(arr))
+        if arr[i] > max_val:
+            max_val = arr[i]
+    return max_val
