e48ced4c3787ca9684c42d0695a4923335cbe5fd
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-26 12:11:51 +0300

    array5

---
+++ Arrays/Onedimensionalarray.py
@@ -0,0 +1,18 @@
+#declare and initialize the array with student grades
+
+grades = [90, 80, 70, 60, 50, 40, 30, 20, 10, 0]
+
+#print the entire array
+
+print(grades)
+
+#print the grade of the student at index 3
+
+print(grades[3])
+
+
+#update the grade of the studet at index 3
+
+grades[3] = 456
+
+print(grades)
