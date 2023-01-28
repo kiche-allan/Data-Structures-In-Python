100d37fa2645fd347180707181d6d3cfe5fe4046
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-29 00:23:13 +0300

    arrays7

--- Arrays/leetcode-questions/4-Median of Two Sorted Arrays.py
+++ Arrays/leetcode-questions/4-Median of Two Sorted Arrays.py
@@ -8,7 +8,16 @@
 
 # Overall the algorithm is called as Median of Two Sorted Array, which can be solved using the variant of binary search called Binary Search with variable length.
 
-
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        
+        half = total //2
+        
+        if len(B) < len(A):
+            A, B = B, A
+        
 
 
 
