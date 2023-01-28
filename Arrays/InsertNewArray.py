20f64b0dd98642917e5dfa125422eaa9fcc76f66
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-28 18:15:08 +0300

    array4

--- Arrays/InsertNewArray.py
+++ Arrays/InsertNewArray.py
@@ -3,4 +3,11 @@ from array import *
 arr1 = array('i', [1, 2, 3, 4, 5, 6])
 arr2 = array('d', [1, 3, 1.5, 1.6])
 
-print(arr2)
+
+#inserting an element in the end of an array
+arr1.insert(6, 7)
+
+#inserting an element in the beginning of an array
+arr1.insert(2, 9)
+
+print(arr1)
