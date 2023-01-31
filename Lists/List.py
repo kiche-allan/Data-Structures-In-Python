4b3dffc5d070b375a9355ea31edbeb0d6987ccce
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-31 10:55:16 +0300

    lists3

--- Lists/List.py
+++ Lists/List.py
@@ -3,6 +3,22 @@ shoppingList = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
 #accessing the elements in the list
 print(shoppingList[0])
 print(shoppingList[4])
+print(shoppingList[-3])
 
 #check if an elemt is in the list
 print("banana" in shoppingList)
+
+
+#traversing the elements in the list using a loop
+
+for i in shoppingList:
+    print(i)
+
+    #traverse in reverse order
+    for i in shoppingList[::-1]:
+        print(i)
+        
+#traverse using the range and len function
+for i in range(len(shoppingList)):
+    shoppingList[i] = shoppingList[i] + "!"
+    print(shoppingList[i])
