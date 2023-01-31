7304c5f6119b2aa6d64118521228f500dcaf24a3
Allan Kiche <tle2819@students.mu.ac.ke>
2023-01-31 10:49:21 +0300

    list2

---
+++ Lists/List.py
@@ -0,0 +1,8 @@
+shoppingList = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
+
+#accessing the elements in the list
+print(shoppingList[0])
+print(shoppingList[4])
+
+#check if an elemt is in the list
+print("banana" in shoppingList)
