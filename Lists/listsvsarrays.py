1f80f1d8189faf76f158d5b87d8b2bbda841f337
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-02 15:38:44 +0300

    arrays7

--- Lists/listsvsarrays.py
+++ Lists/listsvsarrays.py
@@ -2,3 +2,13 @@
 #Both data structures are mutable
 #both can be indexed and iterated through
 #They can be both sliced. 
+
+# arrays vs listMethods()
+
+import numpy as np
+
+myArray = np.array(1, 2, 3, 4, 5, 6)
+myList = [1,2,3,4,5]
+
+print(myArray/2)
+print(myList)
