781299fdde03aaf65eb65265d52973128b353268
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-02 16:29:11 +0300

    dictionaries

---
+++ Dictionary/dictionary.py
@@ -0,0 +1,13 @@
+#creating a dictionary
+phone_book = {'John': 123456789, 'Jack': 987654321, 'Jill': 456789123}
+
+#accessing the value for a key
+print(phone_book['John'])
+
+#adding a new key-value pair
+phone_book['Jack'] = '555-333'
+
+#updating a value for a key
+phone_book['Jill'] = '555-2468'
+
+#removing
