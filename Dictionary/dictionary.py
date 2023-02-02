490168bf3b0143fa90b818c09fa8e33b43dec0e8
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-02 16:33:11 +0300

    dict2

--- Dictionary/dictionary.py
+++ Dictionary/dictionary.py
@@ -10,4 +10,15 @@ phone_book['Jack'] = '555-333'
 #updating a value for a key
 phone_book['Jill'] = '555-2468'
 
-#removing
+#removing a key-value pair
+del phone_book['John']
+
+#checking if a key exists in the dictionary
+
+if 'Jack' in phone_book:
+    print("Jack's phone number is ", phone_book['Jack'])
+    
+#iterating over a dictionary
+
+for name in phone_book:
+    print(name, phone_book[name])
