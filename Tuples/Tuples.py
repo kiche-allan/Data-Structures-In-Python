01702d5de3d54e00ee0c49fef32ba17b2fee35a8
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-03 16:53:09 +0300

    tupple1

---
+++ Tuples/Tuples.py
@@ -0,0 +1,12 @@
+#how to create a tuple
+newTuple = 'a', 'b', 'c', 'd', 'e'
+
+newTuple1 = ('a')
+print(newTuple1)
+print(newTuple)
+
+#using built in tuple function
+newTuple3 = tuple()
+print(newTuple3)
+
+#the time complexity for creating a tuple is 0(1) because here we are defining a ll the values at once/ up front.
