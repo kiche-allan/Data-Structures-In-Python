89b0b29cbcdc39a0761644cbf5e4cb8fe164100a
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-10 21:46:24 +0300

    linked4

--- LinkedList/linkedsample.py
+++ LinkedList/linkedsample.py
@@ -6,13 +6,22 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-        
+     
+
+#assign item values
+   
 linked_list = LinkedList()
 linked_list.head = Node(1)
 second = Node(2)
 third = Node(3)
 
+#connect nodes
+
 linked_list.head.next = second
 second.next = third
 
+#print the linked list item
+while linked_list.head != None:
+    print(linked_list.head.data, end= " ")
+    linked_list.head = linked_list.head.next
 # In this example, the Node class represents a single node in the linked list, with a data attribute that stores the node's value and a next attribute that stores a reference to the next node in the list. The LinkedList class represents the linked list itself, with a head attribute that stores a reference to the first node in the list
