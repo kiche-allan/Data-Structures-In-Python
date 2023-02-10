1ddf163cf3d8d76cdab49a17d34113f2da6bbd97
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-11 00:34:33 +0300

    singly3

--- LinkedList/Singly.py
+++ LinkedList/Singly.py
@@ -8,3 +8,12 @@ class LinkedList:
         self.head = None
         self.tail = None
         self.size = 0
+        
+singly_linked_list = LinkedList()
+node1 = Node(1)
+node2 = Node(2)
+
+#connect the nodes
+singly_linked_list.head = node1
+singly_linked_list.head.next = node2
+singly_linked_list.tail = node2
