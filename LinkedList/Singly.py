509e48ae4676bffc55b23e995da06aecd43d79b3
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-11 00:30:10 +0300

    singly

---
+++ LinkedList/Singly.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self) -> None:
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
+        self.size = 0
