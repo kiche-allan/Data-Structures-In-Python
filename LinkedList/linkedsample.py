c96a17f006d48e9459a0fd8860885993f1b4af5a
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-10 21:19:48 +0300

    linked3

--- LinkedList/linkedsample.py
+++ LinkedList/linkedsample.py
@@ -14,3 +14,5 @@ third = Node(3)
 
 linked_list.head.next = second
 second.next = third
+
+# In this example, the Node class represents a single node in the linked list, with a data attribute that stores the node's value and a next attribute that stores a reference to the next node in the list. The LinkedList class represents the linked list itself, with a head attribute that stores a reference to the first node in the list
