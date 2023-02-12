53f7809f0a712b096fd3ec1bb709e46098115de7
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-12 23:21:00 +0300

    delete node

--- LinkedList/Singly.py
+++ LinkedList/Singly.py
@@ -17,3 +17,16 @@ node2 = Node(2)
 singly_linked_list.head = node1
 singly_linked_list.head.next = node2
 singly_linked_list.tail = node2
+
+#delete a node from singly linked list
+def deleteNode(self, location):
+    if self.head is None:
+        print('The SSL does not exist')
+        
+    else:
+        if location == 0:
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            else:
+                self.head = self.head.next
