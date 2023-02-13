7ff81ad5e599f3975f4ab9e18ae02dbfb51b5d53
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-13 14:28:22 +0300

    stackedlist

--- Stacks/StackLinkedlist.py
+++ Stacks/StackLinkedlist.py
@@ -14,6 +14,12 @@ class Stack:
     def __init__(self) -> None:
         self.LinkedList = LinkedList()
         
+        #make the function iterable
+    def __iter__(self):
+        curNode = self.head
+        while curNode:
+            curNode = curNode.next
+        
     def isEmpty(self):
         if self.LinkedList.head == None:
             return True
