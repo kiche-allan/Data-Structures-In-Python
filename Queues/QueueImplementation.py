8a7f094ffb7f5ec70a6bd6ded351cd6ad125db09
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-13 18:27:52 +0300

    enqueue2

--- Queues/QueueImplementation.py
+++ Queues/QueueImplementation.py
@@ -11,3 +11,19 @@ class Queue:
     def dequeue(self):
         if len(self.queue) < 1:
             return None
+        
+        return self.queue.pop(0)
+    
+    #display the queue
+    def display(self):
+        print(self.queue)
+        
+    def size(self):
+        return len(self.queue)
+    
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+q.enqueue(5)
