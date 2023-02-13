0ff315f8e4b55b7b560456ee12669065c256586f
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-13 14:14:54 +0300

    stacklinkedlist

---
+++ Stacks/StackLinkedlist.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+        
+        
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
+        self.size = 0
+        
+class Stack:
+    def __init__(self) -> None:
+        self.LinkedList = LinkedList()
+        
