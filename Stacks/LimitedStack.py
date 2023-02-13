5c37032999b077c278be2d42c9f191ef4b2fa589
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-13 11:41:19 +0300

    limited stacks

---
+++ Stacks/LimitedStack.py
@@ -0,0 +1,9 @@
+class Stack:
+    def __init__(self) -> None:
+        self.maxSize = maxsize
+        self.list = []
+        
+    def __str__(self):
+        values = self.list.reverse()
+        values = [str(x) for x in self.list]
+        return '\n'.join(values)
