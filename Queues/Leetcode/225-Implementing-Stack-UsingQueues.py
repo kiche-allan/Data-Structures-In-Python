d4676076513217046f4a931e7d13bfadb39ce3f0
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-15 08:55:47 +0300

    leetcode3

--- Queues/Leetcode/225-Implementing-Stack-UsingQueues.py
+++ Queues/Leetcode/225-Implementing-Stack-UsingQueues.py
@@ -1,1 +1,20 @@
-In implementing a stack with quesues, we can only implement 0(1) when pushing but 0(n) when popping time complexities. This is because we have to pop all the elements from the queue and push them back to the queue except the last element. This is done by using a temporary queue to store the elements. We can also implement 0(1) when popping but 0(n) when pushing time complexities. This is because we have to pop all the elements from the queue and push them back to the queue except the last element. This is done by using a temporary queue to store the elements.
+# In implementing a stack with quesues, we can only implement 0(n) when pushing but 0(1) when popping time complexities. This is because we have to pop all the elements from the queue and push them back to the queue except the last element. This is done by using a temporary queue to store the elements. We can also implement 0(1) when popping but 0(n) when pushing time complexities. This is because we have to pop all the elements from the queue and push them back to the queue except the last element. This is done by using a temporary queue to store the elements.
+
+class MyStack:
+    def __init__(self):
+        self.q = deque()
+        
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        
+    def pop(self) -> int:
+        
+        
+    def top(self) -> int:
+        return self.q[-1]
+        
+    
+    def empty(self) -> bool:
+        return len(self.q) == 0
+   
+            
