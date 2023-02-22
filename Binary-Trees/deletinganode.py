bf11711b8a53aebf93e40fdce7440009c1e5cfaa
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-22 10:37:33 +0300

    deletinganode

---
+++ Binary-Trees/deletinganode.py
@@ -0,0 +1,12 @@
+# To delete a node from a binary tree, you need to perform the following steps:
+
+# Find the node to delete in the binary tree.
+# If the node has no children, simply delete it by updating its parent to remove the reference to the node.
+# If the node has one child, replace the node with its child by updating its parent to point to the child.
+# If the node has two children, find the node's successor (i.e., the smallest node in the node's right subtree) and replace the node's value with the successor's value. Then delete the successor node.
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
