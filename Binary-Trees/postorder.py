bed869427ca51cf45c35fa0273246db5706f6abb
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-22 08:34:20 +0300

    postorder2

--- Binary-Trees/postorder.py
+++ Binary-Trees/postorder.py
@@ -14,3 +14,22 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+        
+        
+def postorderTraversal(root: TreeNode) -> List(int):
+    if not root:
+        return []
+    
+    result = []
+    stack = [root]
+    
+    while stack:
+        node = stack.pop()
+        result.append(node.val)
+        
+        if node.left:
+            stack.append(node.left)
+        if node.right:
+            stack.append(node.right)
+            
+    return result[::-1]
