3862e46dc475cdf4179f0b4c3e9e77b821807397
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-22 09:58:49 +0300

    levelorder1

--- Binary-Trees/leveloder.py
+++ Binary-Trees/leveloder.py
@@ -6,3 +6,11 @@
 # Visit all the nodes at level 3 (the great-grandchildren of the root node).
 # And so on, until all the nodes in the tree have been visited.
 # In other words, the level order traversal visits the nodes in order of increasing depth, from top to bottom and from left to right.
+
+# Level order traversal is useful in a variety of binary tree problems, such as finding the minimum depth of a binary tree or finding the leftmost or rightmost node in a binary tree.
+
+class TreeNode:
+    def __init__(self, val=0, left= None, right= None):
+        self.val = val
+        self.left = left
+        self.right = right
