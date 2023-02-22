8db94f377ee9b4b54aa651a899ec1f8910a34f81
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-22 10:04:46 +0300

    levelorder3

--- Binary-Trees/leveloder.py
+++ Binary-Trees/leveloder.py
@@ -14,3 +14,24 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+        
+def levelOrderTraversal(root: TreeNode) -> List[int]:
+    if not root:
+        return []
+    
+    result = []
+    queue = [root]
+    
+    while queue:
+        level_size = len(queue)
+        level_nodes = []
+        
+        for i in range(level_size):
+            node = queue.pop(0)
+            level_nodes.append(node.val)
+            
+            if node.left:
+                queue.append(node.left)
+                
+            if node.right:
+                queue.append(node.right)
