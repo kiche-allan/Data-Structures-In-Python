77cff6e06d8b53b4b74faf992b8c2c70f209f85f
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-22 10:24:08 +0300

    search1

---
+++ Binary-Trees/searchingforanode.py
@@ -0,0 +1,7 @@
+# To search for a node in a binary tree, you can use a depth-first search algorithm, such as a recursive approach. Here's how you can implement it in Python:
+
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
