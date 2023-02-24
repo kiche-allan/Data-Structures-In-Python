489d80702d2368a9adee8f1d7742d401ef4aa121
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-24 22:38:33 +0300

    enqueue

--- Binary-Trees/binarytree2.py
+++ Binary-Trees/binarytree2.py
@@ -5,3 +5,17 @@ class BinaryTree:
         self.maxSize = size
         
 newBT = BinaryTree(8)
+
+
+def getDeepestNode(rootNode):
+    if not rootNode:
+        return
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not(customQueue.isEmpty()):
+            rootNode = customQueue.dequeue()
+            if rootNode.leftChild:
+                customQueue.enqueue(rootNode.leftChild)
+            if rootNode.rightChild:
+                customQueue.enqueue(rootNode.rightChild)
