58e6139dd21361b9c6751eacc0d119188aa24554
Allan Kiche <tle2819@students.mu.ac.ke>
2023-03-06 11:25:05 +0300

    heap78

--- Binary Heap/binaryheap.py
+++ Binary Heap/binaryheap.py
@@ -4,4 +4,10 @@ class Heap:
         self.customiList = (size +1 ) * [None]
         self.heapSize = 0
         self.maxSize = size +1
+        
+def peekToHeap(rootNode):
+    if not rootNode:
+        return
+    else:
+        return rootNode.customiList[1]
 newBinaryHeap = Heap(5)
