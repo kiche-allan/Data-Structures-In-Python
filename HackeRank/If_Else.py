1c231ed1ac5042c3642fd5ab35c6517c516f5bbb
Allan Kiche <tle2819@students.mu.ac.ke>
2023-03-06 13:01:19 +0300

    hack1

--- HackeRank/If_Else.py
+++ HackeRank/If_Else.py
@@ -1,9 +1,28 @@
-# Given an integer, , perform the following conditional actions:
+# Given an integer,n, perform the following conditional actions:
 
-# If  is odd, print Weird
-# If  is even and in the inclusive range of  to , print Not Weird
-# If  is even and in the inclusive range of  to , print Weird
-# If  is even and greater than , print Not Weird
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If n is even and greater than 20, print Not Weird
 # Input Format
 
 # A single line containing a positive integer, .
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n % 2 ==1:
+        print("Weird")
+    elif n in range(2, 6):
+        print("Not Weird")
+    elif n in range(6, 21):
+        print("Weird")
+    else:
+        print("Not Weird")
