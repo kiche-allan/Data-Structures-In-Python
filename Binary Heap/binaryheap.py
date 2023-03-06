1853867335a6de6a8df3faa3173670d3f26e9300
Allan Kiche <tle2819@students.mu.ac.ke>
2023-03-06 12:03:29 +0300

    level order

--- Binary Heap/binaryheap.py
+++ Binary Heap/binaryheap.py
@@ -10,5 +10,16 @@ def peekToHeap(rootNode):
         return
     else:
         return rootNode.customList[1]
+    
+    #level order traversal for binary heap
+def levelOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    else:
+        for i in range(1, rootNode, heapSize +1):
+            print(rootNode.customList[i])
 newBinaryHeap = Heap(5)
 print(sizeofHeap(newBinaryHeap))
+
+
+
